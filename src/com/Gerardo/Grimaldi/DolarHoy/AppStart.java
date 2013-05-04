db5494bbd5c1bf04ae4de12f2cc559abe63a4510
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2013-05-04 12:12:22 -0300

    functional ui

--- src/com/Gerardo/Grimaldi/DolarHoy/AppStart.java
+++ src/com/Gerardo/Grimaldi/DolarHoy/AppStart.java
@@ -1,49 +1,28 @@
 package com.Gerardo.Grimaldi.DolarHoy;
 
-import android.R;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import com.viewpagerindicator.PageIndicator;
 import com.viewpagerindicator.TabPageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
 
 public class AppStart extends FragmentActivity {
-    private static final String[] CONTENT = new String[] { "Dolar", "Blue" ,"Calculadora", "Tarjeta", "Mapa", "Contacto", "Acerca de..." };
+    TestFragmentAdapter mAdapter;
+    ViewPager mPager;
+    PageIndicator mIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.Gerardo.Grimaldi.DolarHoy.R.layout.dolar);
+        setContentView(R.layout.main);
 
-        FragmentPagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
+        mAdapter = new TestFragmentAdapter(getSupportFragmentManager());
 
-        ViewPager pager = (ViewPager)findViewById(com.Gerardo.Grimaldi.DolarHoy.R.id.pager);
-        pager.setAdapter(adapter);
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
 
-        TabPageIndicator indicator = (TabPageIndicator)findViewById(com.Gerardo.Grimaldi.DolarHoy.R.id.indicator);
-        indicator.setViewPager(pager);
-    }
-
-    class GoogleMusicAdapter extends FragmentPagerAdapter {
-        public GoogleMusicAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            return FragmentFactory.newInstance(CONTENT[position % CONTENT.length]);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return CONTENT[position % CONTENT.length].toUpperCase();
-        }
-
-        @Override
-        public int getCount() {
-            return CONTENT.length;
-        }
+        mIndicator = (TabPageIndicator)findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
     }
 }
---
+++ src/com/Gerardo/Grimaldi/DolarHoy/Model/Model.java
@@ -0,0 +1,11 @@
+package com.Gerardo.Grimaldi.DolarHoy.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Milo
+ * Date: 04/05/13
+ * Time: 11:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Model {
+}
